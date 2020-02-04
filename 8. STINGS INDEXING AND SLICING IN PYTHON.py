65a93cb5dbbf192f26b68e856046a75b9482aa24
KanwalSaeed <kanwal_saeed122@hotmail.com>
2020-02-04 23:02:04 +0500

    first code

---
+++ 8. STINGS INDEXING AND SLICING IN PYTHON.py
@@ -0,0 +1,40 @@
+
+
+                #0123456789
+student_Name = "Ali Ashraf"
+
+#INDEXING
+print(student_Name[0])
+print(student_Name[1])
+print(student_Name[2])
+print(student_Name[3])
+print(student_Name[4])
+print(student_Name[5])
+print(student_Name[6])
+print(student_Name[7])
+print(student_Name[8])
+print(student_Name[9])
+
+#SLICIND
+print(student_Name[0:3])
+print(student_Name[4:10])
+
+# by default starting value is 0 and final value last indexing value + 1 )
+print(student_Name[:])
+
+
+#NEGATIVE INDEXING
+print(student_Name[-1])
+print(student_Name[-2])
+print(student_Name[-3])
+print(student_Name[-4])
+print(student_Name[-5])
+print(student_Name[-6])
+print(student_Name[-7])
+print(student_Name[-8])
+print(student_Name[-9])
+print(student_Name[0])
+print(student_Name[-10])
+
+print(student_Name[-10:-1])
+print(student_Name[-1:-9])
